9c07e4027c522d2b98eca1d314f87daed24ded82
Jean <jeanc16rlos@gmail.com>
2023-04-15 17:48:48 -0400

    ✨ Import micro-frontend-1 and microfrontend-2

--- root-config/gatsby-config.ts
+++ root-config/gatsby-config.ts
@@ -9,7 +9,10 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    "micro-frontend-1",
+    "micro-frontend-2",
+  ],
 }
 
 export default config
